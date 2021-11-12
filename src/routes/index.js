dfe0464c990abd422c02188d103893374b16772e
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-11 23:12:57 -0300

    Added the axios request for the api of nfl and send the json data for the end point /partidas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const router = new Router();
 
@@ -7,7 +8,17 @@ router.get('/', (req, res) => {
 });
 
 router.get('/partidas', (req, res) => {
-  res.send('PARTIDAS');
+  axios.get('https://api.sportsdata.io/v3/nfl/scores/json/TeamGameStats/2021/8', {
+    headers: {
+      'Ocp-Apim-Subscription-Key': 'c00c2adab1cb4775965c58a43cb3e5f5',
+    },
+  })
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get('/estatisticas', (req, res) => {
