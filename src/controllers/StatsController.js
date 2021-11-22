5a37355809e9437e5cba4793dd382134634dcae5
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-22 20:36:43 -0300

    Created the logic for the Stats controller, and made the response for the rest api with correct datas

--- src/controllers/StatsController.js
+++ src/controllers/StatsController.js
@@ -1,6 +1,46 @@
+import axios from 'axios';
+import 'dotenv/config';
+
 class StatsController {
   index(req, res) {
-    res.send('Estatisticas');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    axios.get('https://api.sportsdata.io/v3/nfl/scores/json/TeamGameStats/2021/11', {
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.SECRET_API_KEY,
+      },
+    })
+      .then((response) => {
+        const stats = [];
+        response.data.forEach((el) => {
+          stats.push({
+            TeamName: el.TeamName,
+            OffensivePlays: el.OffensivePlays,
+            OffensiveYards: el.OffensiveYards,
+            OffensiveYardsPerPlay: el.OffensiveYardsPerPlay,
+            Touchdowns: el.Touchdowns,
+            PassingYards: el.PassingYards,
+            PassingTouchdowns: el.PassingTouchdowns,
+            PassingInterceptions: el.PassingInterceptions,
+            CompletionPercentage: el.CompletionPercentage,
+            Penalties: el.Penalties,
+            Fumbles: el.Fumbles,
+            TimesSacked: el.TimesSacked,
+            QuarterbackHits: el.QuarterbackHits,
+            Kickoffs: el.Kickoffs,
+            Sacks: el.Sacks,
+            PassingDropbacks: el.PassingDropbacks,
+            FirstDowns: el.FirstDowns,
+          });
+        });
+        res.send(stats);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
