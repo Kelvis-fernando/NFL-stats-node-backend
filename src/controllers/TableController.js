7492d6f8b2220041481dc7ecb1a0e4b66958206e
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-11-23 19:14:17 -0300

    Created the end point tabela, and consuming the datas of the api of NFL, filter the must important datas that i need, and created the logic for a response for the rest api

--- src/controllers/TableController.js
+++ src/controllers/TableController.js
@@ -1,6 +1,35 @@
+import axios from 'axios';
+import 'dotenv/config';
+
 class TableController {
   index(req, res) {
-    res.send('Tabela');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    axios.get('https://api.sportsdata.io/v3/nfl/scores/json/Standings/2021', {
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.SECRET_API_KEY,
+      },
+    })
+      .then((response) => {
+        const table = [];
+        response.data.forEach((el) => {
+          table.push({
+            Name: el.Name,
+            Conference: el.Conference,
+            Division: el.Division,
+            Wins: el.Wins,
+            Losses: el.Losses,
+            Ties: el.Ties,
+          });
+        });
+        res.send(table);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
