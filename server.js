fbd9f97709cdccf00c01939ecfd6526a2a526df8
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-12-13 19:50:09 -0300

    Created the response data for end point /

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app';
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
-  console.log('http://localhost:3001/');
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+  console.log('http://localhost:3000/');
 });
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,22 @@
+import Axios from 'axios';
+
 class HomeController {
   index(req, res) {
-    res.send('Home');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    Axios.get('https://api.sportsdata.io/v3/nfl/scores/json/Teams', {
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.SECRET_API_KEY,
+      },
+    })
+      .then((response) => {
+        res.send(response.data);
+      }, (error) => {
+        console.log(error);
+      });
   }
 }
 
